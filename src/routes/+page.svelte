dfaed2a614bad6da56aab1bad813bfc876c01f5c
bobbyZ94 <r.zioltkowski@gmail.com>
2023-05-30 22:19:28 +0200

    added icones

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script>
-	import { Alert } from 'flowbite-svelte';
+	import { Alert } from 'flowbite-svelte'
+	import IconAccountBox from '~icons/mdi/account-box'
 </script>
 
 <div class="p-8">
+	<IconAccountBox class="h-10 w-5" />
 	<Alert>
 		<span class="font-medium">Info alert!</span> Change a few things up and try submitting again.
 	</Alert>
