7093a50dca1bf0dd2c63e8922f1f0bcdde871759
Robert Zioltkowski <r.zioltkowski@gmail.com>
2023-07-02 17:34:14 +0200

    feat: modal for gallery with description

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,23 +1,30 @@
 <script lang="ts">
 	import { Gallery } from 'flowbite-svelte'
-	import { Modal } from 'flowbite-svelte'
 	import { fade } from 'svelte/transition'
 	import Close from '~icons/mdi/close-circle-outline'
 	export let data
 
+	const imageData = data.gallery.gallerie_bild
+	console.log('IMGDATA', imageData)
+
 	// Modal data
 	let imageModal = false
 	let imageSrc = ''
+	$: console.log('SRC', imageSrc)
 	let imageAlt: string | undefined = ''
-	let imageDescription = ''
+	$: imageDescription = imageSrc
+		? imageData.filter((image) => image.item.gallerie_bild === imageSrc)[0].item
+				.gallerie_bild_beschreibung
+		: ''
 
+	console.log('DESC', imageDescription)
 	const images: { src: string; alt: string }[] = []
 
 	const masonry_columns = 3
 
-	data.gallery.gallerie_bild.forEach((item: any) => {
+	imageData.forEach((item: any) => {
 		images.push({
-			src: `https://directus.lifecreates.art/assets/${item.item.gallerie_bild}`,
+			src: item.item.gallerie_bild,
 			alt: item.item.gallerie_bild_name
 		})
 	})
@@ -54,7 +61,7 @@
 				}}
 			>
 				<img
-					src={item.src}
+					src={`https://directus.lifecreates.art/assets/${item.src}`}
 					alt={item.alt}
 					class="h-full max-w-full rounded object-cover drop-shadow-lg"
 				/>
@@ -71,8 +78,28 @@
 		on:click|self={() => (imageModal = false)}
 		class="fixed left-0 top-0 z-20 flex h-screen w-screen flex-col items-center justify-center bg-black/80"
 	>
-		<Close class="fixed right-10 top-10 text-white" style="font-size: 28px" />
-
-		<img src={imageSrc} alt={imageAlt} class="max-h-[80%] max-w-[80%]" />
+		<button on:click|self={() => (imageModal = false)}>
+			<Close class="fixed right-8 top-8 cursor-pointer text-white" style="font-size: 28px" />
+		</button>
+		<div
+			on:click|self={() => (imageModal = false)}
+			class="flex flex-col items-center justify-center gap-5"
+		>
+			<img
+				src={`https://directus.lifecreates.art/assets/${imageSrc}`}
+				alt={imageAlt}
+				class="max-h-[40rem] max-w-[22rem] rounded drop-shadow-lg md:max-w-[33rem] lg:max-w-[44rem] xl:max-w-[55rem]"
+			/>
+			<div class="translate-x-2 translate-y-2 rounded bg-gray-400">
+				<div class="max-w-fit -translate-x-2 -translate-y-2 rounded bg-white p-3 drop-shadow-lg">
+					<div class="rounded font-bold uppercase underline underline-offset-4">
+						{imageAlt}
+					</div>
+					<div class="prose prose-sm">
+						{@html imageDescription}
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 {/if}
