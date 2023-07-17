b5ee65a856256c21e8794e46acb037cafa80e15b
Robert Zioltkowski <r.zioltkowski@gmail.com>
2023-07-17 11:50:15 +0200

    feat: added contact page with contact form; implemented superforms validation and nodemailer transport; implemented error handling;

--- src/routes/contact/+page.server.ts
+++ src/routes/contact/+page.server.ts
@@ -2,60 +2,73 @@ import { directus } from '../../directus'
 import { z } from 'zod'
 import { superValidate } from 'sveltekit-superforms/server'
 import { createTransport, type Transporter } from 'nodemailer'
+import { error, fail } from '@sveltejs/kit'
+import { env } from '$env/dynamic/private'
 import type { PageServerLoad, Actions } from './$types'
 
 const schema = z.object({
-	name: z.string(),
+	name: z.string().min(2).max(128),
 	email: z.string(),
-	text: z.string(),
+	text: z.string().min(2).max(2000),
 	conditions: z.string()
 })
 
 let transport: Transporter
 
 export const actions = {
-	default: async ({ request }) => {
-		// Get form (already validated on client side with browser validation)
+	sendEmail: async ({ request }) => {
+		// Serverside superform api
 		const form = await superValidate(request, schema)
 
-		// Create nodemailer transporter
-		if (!transport) {
-			transport = createTransport(
-				{
-					host: 'mail.privateemail.com',
-					port: 587,
-					secure: false, // true for 465, false for other ports
-					auth: {
-						user: 'info@lifecreates.art',
-						pass: 'aDJlGgYR4buFL6rTslt0'
-					}
-				},
-				{
-					from: 'contactform@lifecreates.art' // sender address
-				}
-			)
+		// Convenient validation check:
+		if (!form.valid) {
+			// Again, always return { form } and things will just work.
+			return fail(400, { form })
+		}
 
-			// Send mail
-			try {
-				await transport.sendMail({
-					to: 'info@lifecreates.art', // just the one person who's getting this email
-					subject: 'Nachricht von Lifecreatesart Homepage', // Subject line
-					text: `Eine Nachricht von ${form.data.name} mit der E-Mail Adresse ${form.data.email}.`, // plain text body
-					html: `<p>Name: ${form.data.name}</p><p>Email: ${form.data.email}</p><p>${form.data.text}</p>` // html body
-				})
-				return { success: 'Message sent successfully!' }
-			} catch (error) {
-				console.log(error)
-				return {
-					error: `ERROR: Message delivery failed due to error. Please try again later or write me an email.`
+		// Create mail transporter
+		transport = createTransport(
+			{
+				host: env.MAIL_HOST,
+				port: 587,
+				secure: false, // true for 465, false for other ports
+				auth: {
+					user: env.MAIL_USER,
+					pass: env.MAIL_PW
 				}
+			},
+			{
+				from: 'contactform@lifecreates.art'
 			}
+		)
+
+		// Send mail
+		try {
+			await transport.sendMail({
+				to: 'info@lifecreates.art',
+				subject: 'Nachricht von Lifecreatesart Homepage',
+				text: `Eine Nachricht von ${form.data.name} mit der E-Mail Adresse ${form.data.email}.`,
+				html: `<p>Name: ${form.data.name}</p><p>Email: ${form.data.email}</p><p>${form.data.text}</p>`
+			})
+			// Return form if sending mail successfull. This starts the event onUpdated and enables success message.
+			return { form }
+		} catch (e) {
+			console.log(e)
+			// Throw error which gets picked up by onError and display error message.
+			throw error(500, {
+				message: 'Error while sending the message. Please try again later or write me an email.'
+			})
 		}
 	}
 } satisfies Actions
 
 export const load = (async ({ params }) => {
+	// Server API:
+	const form = await superValidate(schema)
+
+	// Always return { form } in load AND form actions.
 	return {
-		contact: (await directus.singleton('kontakt').read()) as Kontakt
+		contact: (await directus.singleton('kontakt').read()) as Kontakt,
+		form
 	}
 }) satisfies PageServerLoad
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,99 +1,100 @@
 <script lang="ts">
 	import { Input, Label, Textarea, Checkbox, A, Spinner } from 'flowbite-svelte'
-	import type { ActionData, Snapshot } from './$types'
+	import type { PageData } from './$types'
+	import { superForm } from 'sveltekit-superforms/client'
 
-	let formData = {
-		name: '',
-		email: '',
-		text: '',
-		conditions: false
-	}
+	export let data: PageData
 
-	export const snapshot: Snapshot = {
-		capture: () => formData,
-		restore: (value) => (formData = value)
-	}
-	export let data
-	export let form: ActionData
-
-	let disabled = false
+	// Client superform api
+	let success: boolean = false
+	let error: string = ''
+	const { form, enhance, errors, constraints, capture, restore, delayed } = superForm(data.form, {
+		applyAction: true,
+		invalidateAll: true,
+		resetForm: false,
+		clearOnSubmit: 'errors-and-message',
+		multipleSubmits: 'prevent',
+		onError({ result }) {
+			error = result.error.message
+		},
+		onUpdated: () => (success = true)
+	})
+	// Snapshot form data
+	export const snapshot = { capture, restore }
 </script>
 
-<div class="my-20 max-w-5xl overflow-hidden rounded-lg p-5 drop-shadow-md">
-	{#if form?.success}
-		<div class="flex flex-col gap-3 text-center text-xl font-bold tracking-wide md:text-2xl">
-			<div>{form.success}</div>
-			<div>Thank you for contacting me.</div>
-		</div>
-	{:else if form?.error}
-		{form.error}
+<div class="my-20 max-w-5xl overflow-hidden rounded-lg p-5 drop-shadow-md md:my-32">
+	{#if success}
+		<div class="text-xl">Message sent successfully.</div>
+		<div class="text-xl">Thank you for contacting me!</div>
+	{:else if $delayed}
+		<Spinner color="blue" size="12" class="text-center" />
+	{:else if error}
+		<div class="text-xl">{error}</div>
 	{:else}
-		<div class="mb-12 text-center text-4xl font-bold md:text-6xl">Kontakt</div>
 		<div
 			class="prose-lg break-words p-2 text-center md:prose-2xl prose-h1:mt-0 prose-h2:mt-0 prose-h3:mt-0 prose-blockquote:border-l-gray-500 marker:prose-ol:text-black marker:prose-ul:text-black"
 		>
 			{@html data.contact.text}
 		</div>
-		<form method="POST">
+		<form method="POST" action="?/sendEmail" use:enhance>
 			<div class="flex flex-col gap-6 text-lg md:text-xl">
 				<div>
 					<Label for="name" class="mb-2 text-lg font-bold md:text-xl">Name</Label>
 					<Input
 						type="text"
 						name="name"
-						minlength="2"
-						maxlength="128"
-						id="name"
-						placeholder="Max Mustermann"
-						required
-						class="text-lg invalid:border-red-300 md:text-xl"
-						bind:value={formData.name}
+						aria-invalid={$errors.name ? 'true' : undefined}
+						bind:value={$form.name}
+						{...$constraints.name}
+						class={`${$errors.name ? 'border border-red-500' : ''} text-lg md:text-xl`}
 					/>
 				</div>
 				<div class="">
 					<Label for="email" class="mb-2 text-lg font-bold md:text-xl">Email</Label>
 					<Input
 						type="email"
-						id="email"
 						name="email"
-						placeholder="max@mustermann.com"
-						required
-						class="text-lg md:text-xl"
-						bind:value={formData.email}
+						aria-invalid={$errors.email ? 'true' : undefined}
+						bind:value={$form.email}
+						{...$constraints.email}
+						class={`${$errors.email ? 'border border-red-500' : ''} text-lg md:text-xl`}
 					/>
 				</div>
 				<div class="">
 					<Label for="text" class="mb-2 text-xl font-bold">Message</Label>
 					<Textarea
-						id="text"
-						required
-						name="text"
+						rows="8"
 						type="text"
-						rows="10"
-						placeholder="Hello ..."
-						class="text-lg md:text-xl"
-						minlength="3"
-						maxlength="3000"
-						bind:value={formData.text}
+						name="text"
+						aria-invalid={$errors.text ? 'true' : undefined}
+						bind:value={$form.text}
+						{...$constraints.text}
+						class={`${$errors.text ? 'border border-red-500' : ''} text-lg md:text-xl`}
 					/>
 				</div>
-				<div class="text-xl">
-					<Checkbox id="conditions" name="conditions" type="checkbox" required>
-						I agree with the&nbsp
+				<div>
+					<Checkbox
+						type="checkbox"
+						name="conditions"
+						aria-invalid={$errors.conditions ? 'true' : undefined}
+						value="agreed"
+						{...$constraints.conditions}
+						class="text-xl"
+					>
+						<span class="text-base">I agree with the&nbsp</span>
 						<A
 							href="/privacy#contactform"
-							class="text-primary-700 hover:underline dark:text-primary-600"
+							class="text-base text-primary-700 hover:underline dark:text-primary-600"
 						>
 							terms and conditions.
 						</A>
 					</Checkbox>
 				</div>
-				<!-- svelte-ignore a11y-no-static-element-interactions -->
-				<!-- svelte-ignore a11y-click-events-have-key-events -->
 				<div class="mt-5 flex justify-center">
-					<button type="submit" class="group rounded-xl bg-gray-400 text-lg text-white md:text-xl"
+					<button class="group rounded-xl bg-gray-400 text-lg text-white md:text-xl"
 						><div
-							class="h-full w-full rounded-xl p-2 drop-shadow-lg valid:bg-gray-800 valid:duration-300 valid:ease-in-out invalid:bg-gray-400 valid:group-hover:-translate-x-2 valid:group-hover:-translate-y-2"
+							class=":bg-gray-800 h-full w-full rounded-xl bg-gray-800 p-2 drop-shadow-lg duration-300 ease-in-out group-hover:-translate-x-2 group-hover:-translate-y-2"
 						>
 							Send message
 						</div></button
